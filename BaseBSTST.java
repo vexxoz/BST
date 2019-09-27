bb7ac4bc467651ee78ac2156d1219cce66b235c2
Blake Caldwell <blakecaldwell123@gmail.com>
2019-09-26 23:10:18 -0700

    All methods implemented

--- BaseBSTST.java
+++ BaseBSTST.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Vector;
+
 /**
  * A binary search tree based implementation of a symbol table.
  * 
  * @author (your name), Sedgewick and Wayne, Acuna
  * @version (version)
  */
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+
         
 public class BaseBSTST<Key extends Comparable<Key>, Value> implements OrderedSymbolTable<Key, Value> {
     private Node root;
@@ -50,6 +53,25 @@ public class BaseBSTST<Key extends Comparable<Key>, Value> implements OrderedSym
         else return x.val;
     }
     
+    public Value getFast(Key key) {
+        Node index = root;
+        while(true) {
+        	if(index == null) {
+        		break;
+        	}
+        	int cmp = key.compareTo(index.key);
+        	if(cmp==0) {
+        		return index.val;
+        	}
+        	if(cmp<0) {
+        		index = index.left;
+        	}else {
+        		index = index.right;
+        	}
+        }
+        return null;
+    }
+    
     @Override
     public void put(Key key, Value val) {
         root = put(root, key, val);
@@ -65,6 +87,44 @@ public class BaseBSTST<Key extends Comparable<Key>, Value> implements OrderedSym
         else x.val = val;
         x.N = size(x.left) + size(x.right) + 1;
         return x;
+    }
+    
+    public void putFast(Key key, Value value) {
+		if(root == null) {
+			root = new Node(key, value, 1);
+			return;
+		}
+		Node index = root, next = root;
+		boolean increment = true;
+		if(contains(key)) {
+			increment = false;
+		}
+    	while(true) {
+    		int cmp = key.compareTo(index.key);
+    		if(increment) {
+    			index.N++;
+    		}
+    		if(cmp<0) {
+    			next = index.left;	
+        		if(next == null) {
+        			index.left = new Node(key, value, 1);
+        			return;
+        		}
+        		index = next;
+    		}
+    		if(cmp>0) {
+    			next = index.right;	
+        		if(next == null) {
+        			index.right = new Node(key, value, 1);
+        			return;
+        		}
+        		index = next;
+    		}
+    		if(cmp==0) {
+    			index.val = value;
+    			return;
+    		}
+    	}
     }
     
     @Override
@@ -220,25 +280,125 @@ public class BaseBSTST<Key extends Comparable<Key>, Value> implements OrderedSym
 
     @Override
     public Key ceiling(Key key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Node x = ceiling(root, key);
+        if (x == null)
+            return null;
+        return x.key;
+    }
+    
+    private Node ceiling(Node x, Key key) {
+        if (x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp > 0) return ceiling(x.right, key);
+        Node t = ceiling(x.left, key);
+        if (t != null) return t;
+        else return x;
     }
     
     @Override
     public void deleteMax() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        root = deleteMax(root);
+    }
+    
+    private Node deleteMax(Node x) {
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
     }
 
     @Override
     public int size(Key lo, Key hi) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    	if(root == null) {
+    		return 0;
+    	}
+        return size(root, lo, hi);
+    }
+    
+    private int size(Node x, Key lo, Key hi) {
+    	if(x == null) return 0;
+    	int cmp = x.key.compareTo(lo);
+    	if(cmp<0) {
+    		return size(x.right, lo, hi);
+    	}
+    	cmp = x.key.compareTo(hi);
+    	if(cmp>0) {
+    		return size(x.left, lo, hi);
+    	}
+    	return size(x.left, lo, hi) + size(x.right, lo, hi) + 1;	
     }
     
     public void balance() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    	Vector<Node> myNodes = new Vector<Node>();
+    	getNodesList(root, myNodes);
+    	root = balance(myNodes, 0, myNodes.size()-1);
+    }
+    
+    private void getNodesList(Node head, Vector<Node> myNodes) {
+        if (head == null) 
+            return; 
+ 
+        getNodesList(head.left, myNodes); 
+        myNodes.add(head); 
+        getNodesList(head.right, myNodes);
+		
+	}
+
+	private Node balance(Vector<Node> nodes, int lo, int hi) {
+    	if(lo > hi) {
+    		return null;
+    	}
+		
+    	int mid = (hi+lo)/2;
+    	Node node = nodes.get(mid); 
+    	
+    	node.left = balance(nodes, lo, mid - 1); 
+    	node.right = balance(nodes, mid + 1, hi); 
+  
+		return node;
     }
     
     public void printLevel(Key key) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    	Queue<Key> myQueue = new LinkedList<>();
+    	int rank = rank(key);
+    	for(int i =0;i<height(root);i++) {
+    		printLevel(root,i,myQueue);
+    	}
+    	printLevel(root, rank, myQueue);
+    	for(Key value : myQueue) {
+    		System.out.print(value + " ");
+    	}
+    	System.out.println();
+    }
+    
+    private int height(Node index)  
+    { 
+        if (index == null) 
+            return 0; 
+
+            int leftHeight = height(index.left); 
+            int rightHeight = height(index.right); 
+   
+            
+            if (leftHeight > rightHeight) 
+                return ++leftHeight; 
+            return ++rightHeight; 
+    } 
+    
+    private void printLevel(Node root, int rank, Queue<Key> queue) {
+    	if(root == null) {
+    		return;
+    	}
+    	
+    	if(rank == 1) {
+    		queue.add(root.key);
+    	}
+    	
+    	printLevel(root.left, rank-1, queue);
+    	printLevel(root.right, rank-1, queue);
+    	
     }
 
     /**
@@ -255,15 +415,42 @@ public class BaseBSTST<Key extends Comparable<Key>, Value> implements OrderedSym
         bst.put(5, "FIVE");
         bst.put(2, "TWO");
         bst.put(7, "SEVEN");
+        bst.put(8, "Eight");
+        
+        System.out.println("rank: " + bst.rank(bst.max()));
+        System.out.println("size: " + bst.size(0,1));
         
         System.out.println("Contains: " + bst.contains(1));
         System.out.println("isEmpty: " + bst.isEmpty());
         
+        System.out.println("get: " + bst.get(55));
+        System.out.println("getFast: " + bst.getFast(55));       
+        
+        // Test PUT
+//        System.out.println("Size: " + bst.size());
+//        bst.put(15, "FIFTEEN");
+//        System.out.println("put_Size: " + bst.size());
+//        bst.putFast(16, "SIXTEEN");
+//        System.out.println("putFast_Size: " + bst.size());
+        
+        // test deleteMax
+//        System.out.println("getMax: " + bst.max());
+//        bst.deleteMax();
+//        System.out.println("Delete_Size: " + bst.size());
+//        System.out.println("Max_Contains: " + bst.contains(16));
+        
+        // Test floor/ceil
+//        System.out.println("Floor: "+  bst.get(bst.floor(6)));
+//        System.out.println("Ceil: "+  bst.get(bst.ceiling(6)));
+        
+        // Test size()
+        System.out.println("Size_Test: "+  bst.size(2,3));
+        
         System.out.println("Before balance:");
-        //bst.printLevel(10); //root
+        bst.printLevel(10); //root
         
         System.out.println("After balance:");
-        //bst.balance();
-        //bst.printLevel(5); //root
+        bst.balance();
+        bst.printLevel(10); //root
     }
 }
